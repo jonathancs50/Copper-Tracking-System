fd70bb818984144fbe14b4cb9b026447793ab412
jonathancs50 <66359052+jonathancs50@users.noreply.github.com>
2024-04-04 09:59:03 +0200

    Adding table delivery

--- routes/deliveryRoutes.js
+++ routes/deliveryRoutes.js
@@ -2,10 +2,35 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-
-    // Render HTML using EJS template
-    res.render("delivery.ejs");
-
+  // Render HTML using EJS template
+  res.render("delivery.ejs");
 });
+// Route handler for inserting a purchase
+// Route handler for fetching purchases and descriptions
+router.get("/delivery", (req, res, next) => {
+  const purchaseQuerySelect = "SELECT * FROM tblPurchase";
+  const descriptionQuerySelect = "SELECT Description FROM tblPriceList";
 
+  // Fetch purchases
+  global.db.all(purchaseQuerySelect, (err, purchases) => {
+    if (err) {
+      next(err);
+      return;
+    }
+
+    // Fetch descriptions
+    global.db.all(descriptionQuerySelect, (err, descriptions) => {
+      if (err) {
+        next(err);
+        return;
+      }
+
+      // Render HTML using EJS template with purchases and descriptions passed to it
+      res.render("delivery", {
+        purchases: purchases,
+        descriptions: descriptions,
+      });
+    });
+  });
+});
 module.exports = router;
