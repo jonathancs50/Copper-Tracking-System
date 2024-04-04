bd6216a780985c73d3853e97b2c1f5ee076a289f
jonathancs50 <66359052+jonathancs50@users.noreply.github.com>
2024-04-04 07:53:13 +0200

    Commenting

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
+// Import necessary modules
 const express = require("express");
-const app = express();
-const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
+const sqlite3 = require("sqlite3").verbose();
+
+// Import routes
 const purchaseRoutes = require("./routes/purchaseRoutes");
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"));
-app.use(express.json());
+// Initialize express app
+const app = express();
 
-const sqlite3 = require("sqlite3").verbose();
+// Set default port or use environment variable
+const port = process.env.PORT || 3000;
+
+// Middleware to parse request bodies
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json()); // For JSON data
+
+// Set EJS as the view engine for rendering views
+app.set("view engine", "ejs");
+
+// Serve static files from the 'public' directory
+app.use(express.static(__dirname + "/public"));
+
+// Database setup
 global.db = new sqlite3.Database("./mainDB.db", function (err) {
   if (err) {
-    console.error(err);
+    console.error(err.message);
     process.exit(1);
   } else {
     console.log("Database connected");
-    global.db.run("PRAGMA foreign_keys=ON");
+    global.db.run("PRAGMA foreign_keys=ON"); // Enable foreign key constraints
   }
 });
 
-
-// Use the purchaseRoutes for the / route
+// Use purchaseRoutes for handling routes starting with "/"
 app.use("/", purchaseRoutes);
 
 // Start the server
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+  console.log(`Server listening on port ${port}`);
 });
-
