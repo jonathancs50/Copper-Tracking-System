3c64350bfcc6c804906b69cf0b3e5b4f170d5da7
Michael Dale <mcdale0100@gmail.com>
2024-04-08 13:43:34 +0200

    count form

--- routes/countRoutes.js
+++ routes/countRoutes.js
@@ -1,22 +1,53 @@
 const express = require("express");
 const router = express.Router();
 
-// Route handler for rendering the delivery page with purchases and descriptions
+// Route handler for fetching purchases and descriptions for form
 router.get("/", (req, res, next) => {
-    const contractNumberSelect =
-      "SELECT DISTINCT ContractNumber FROM tblTransactionHistory";
-  
+    const purchaseQuerySelect = "SELECT * FROM tblCount";
+    const descriptionQuerySelect = "SELECT Description FROM tblPriceList";
+    const contractNumberSelect ="SELECT DISTINCT ContractNumber FROM tblCount";
     // Fetch purchases
-    global.db.all(contractNumberSelect, (err, contractNumbers) => {
+    global.db.all(purchaseQuerySelect, (err, purchases) => {
+        if (err) {
+            next(err);
+            return;
+        }
+        
+        // Fetch descriptions
+        global.db.all(descriptionQuerySelect, (err, descriptions) => {
+            if (err) {
+                next(err);
+                return;
+            }
+
+            global.db.all(contractNumberSelect, (err, contractNumbers) => {
+                if (err) {
+                  next(err);
+                  return;
+                }
+            
+            // Render HTML using EJS template with purchases and descriptions passed to it
+            res.render("count", { purchases: purchases, descriptions: descriptions,contractNumbers: contractNumbers });
+        });
+    });
+});
+});
+
+
+//populates the table with contract number
+router.get("/purchases/:contractNumber", (req, res, next) => {
+    const contractNumber = req.params.contractNumber;
+    const purchaseQuery = "SELECT * FROM tblCount WHERE ContractNumber = ?";
+  
+    // Fetch purchases for the specified contract number
+    global.db.all(purchaseQuery, [contractNumber], (err, purchases) => {
       if (err) {
         next(err);
         return;
       }
   
-      // Render HTML using EJS template with purchases and descriptions passed to it
-      res.render("count", {
-        contractNumbers: contractNumbers,
-      });
+      // Send the fetched purchases as JSON response
+      res.json(purchases);
     });
   });
 
